712c2e5d890afaee3d556c836fadf82454f111ea
Ye Shu <hello@shuye.dev>
2023-01-07 02:53:13 -0500

    gallery: Add photos from 2022/06

--- src/data/GalleryImages.tsx
+++ src/data/GalleryImages.tsx
@@ -1,50 +1,212 @@
+import React from 'react';
 import type { Image } from '../components/Gallery';
 
 const images: Image[] = [
   {
-    src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599',
-    width: 800,
-    height: 599,
+    src: '/img/gallery/2022-06/20220602_162412-srcw.jpg',
+    title: 'Inside High Museum of Art',
+    description:
+      '2022/06/02. Atlanta, Georgia, USA. Taken with Oneplus 8T.\nCopyright © 2022 Ye Shu.',
+    width: 3456,
+    height: 4608,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-06/20220602_162412-srcw.jpg',
+        width: 3456,
+        height: 4608,
+      },
+      {
+        src: '/img/gallery/2022-06/20220602_162412-1920w.jpg',
+        width: 1920,
+        height: 2560,
+      },
+      {
+        src: '/img/gallery/2022-06/20220602_162412-1440w.jpg',
+        width: 1440,
+        height: 1920,
+      },
+      {
+        src: '/img/gallery/2022-06/20220602_162412-1024w.jpg',
+        width: 1024,
+        height: 1365,
+      },
+      {
+        src: '/img/gallery/2022-06/20220602_162412-640w.jpg',
+        width: 640,
+        height: 853,
+      },
+      {
+        src: '/img/gallery/2022-06/20220602_162412-320w.jpg',
+        width: 320,
+        height: 426,
+      },
+    ],
   },
   {
-    src: 'https://source.unsplash.com/Dm-qxdynoEc/800x799',
-    width: 800,
-    height: 799,
+    src: '/img/gallery/2022-06/20220605_002625-srcw.jpg',
+    title: 'A Mural near Tech Walkway in Georgia Tech',
+    description: (
+      <>
+        During a night walk with my friend Yifan. Read more about the mural{' '}
+        <a href="https://news.gatech.edu/features/2021/08/youve-been-gone">
+          here
+        </a>
+        .
+        <br />
+        2022/06/05. Atlanta, Georgia, USA. Taken with Oneplus 8T.
+        <br />
+        Copyright © 2022 Ye Shu.
+      </>
+    ),
+    width: 4000,
+    height: 3000,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-06/20220605_002625-srcw.jpg',
+        width: 4000,
+        height: 3000,
+      },
+      {
+        src: '/img/gallery/2022-06/20220605_002625-1920w.jpg',
+        width: 1920,
+        height: 1440,
+      },
+      {
+        src: '/img/gallery/2022-06/20220605_002625-1440w.jpg',
+        width: 1440,
+        height: 1080,
+      },
+      {
+        src: '/img/gallery/2022-06/20220605_002625-1024w.jpg',
+        width: 1024,
+        height: 768,
+      },
+      {
+        src: '/img/gallery/2022-06/20220605_002625-640w.jpg',
+        width: 640,
+        height: 480,
+      },
+      {
+        src: '/img/gallery/2022-06/20220605_002625-320w.jpg',
+        width: 320,
+        height: 240,
+      },
+    ],
   },
   {
-    src: 'https://source.unsplash.com/qDkso9nvCg0/600x799',
-    width: 600,
-    height: 799,
+    src: '/img/gallery/2022-06/20220610_133628-srcw.jpg',
+    title: 'Random Antennae in Georgia Tech',
+    description:
+      'Some Yagi Antennae and a parabolic antenna on the roof of a gatech building.\n2022/06/10. Atlanta, Georgia, USA. Taken with Oneplus 8T.\nCopyright © 2022 Ye Shu.',
+    width: 4000,
+    height: 3000,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-06/20220610_133628-srcw.jpg',
+        width: 4000,
+        height: 3000,
+      },
+      {
+        src: '/img/gallery/2022-06/20220610_133628-1920w.jpg',
+        width: 1920,
+        height: 1440,
+      },
+      {
+        src: '/img/gallery/2022-06/20220610_133628-1440w.jpg',
+        width: 1440,
+        height: 1080,
+      },
+      {
+        src: '/img/gallery/2022-06/20220610_133628-1024w.jpg',
+        width: 1024,
+        height: 768,
+      },
+      {
+        src: '/img/gallery/2022-06/20220610_133628-640w.jpg',
+        width: 640,
+        height: 480,
+      },
+      {
+        src: '/img/gallery/2022-06/20220610_133628-320w.jpg',
+        width: 320,
+        height: 240,
+      },
+    ],
   },
   {
-    src: 'https://source.unsplash.com/iecJiKe_RNg/600x799',
-    width: 600,
-    height: 799,
+    src: '/img/gallery/2022-06/20220610_134342-srcw.jpg',
+    title: 'Random Antennae in Georgia Tech x2',
+    description:
+      'Some more antennae for base stations on the roof of another gatech building.\n2022/06/10. Atlanta, Georgia, USA. Taken with Oneplus 8T.\nCopyright © 2022 Ye Shu.',
+    width: 3000,
+    height: 4000,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-06/20220610_134342-srcw.jpg',
+        width: 3000,
+        height: 4000,
+      },
+      {
+        src: '/img/gallery/2022-06/20220610_134342-1920w.jpg',
+        width: 1920,
+        height: 2560,
+      },
+      {
+        src: '/img/gallery/2022-06/20220610_134342-1440w.jpg',
+        width: 1440,
+        height: 1920,
+      },
+      {
+        src: '/img/gallery/2022-06/20220610_134342-1024w.jpg',
+        width: 1024,
+        height: 1365,
+      },
+      {
+        src: '/img/gallery/2022-06/20220610_134342-640w.jpg',
+        width: 640,
+        height: 853,
+      },
+      {
+        src: '/img/gallery/2022-06/20220610_134342-320w.jpg',
+        width: 320,
+        height: 426,
+      },
+    ],
   },
   {
-    src: 'https://source.unsplash.com/epcsn8Ed8kY/600x799',
-    width: 600,
-    height: 799,
-  },
-  {
-    src: 'https://source.unsplash.com/NQSWvyVRIJk/800x599',
-    width: 800,
-    height: 599,
-  },
-  {
-    src: 'https://source.unsplash.com/zh7GEuORbUw/600x799',
-    width: 600,
-    height: 799,
-  },
-  {
-    src: 'https://source.unsplash.com/PpOHJezOalU/800x599',
-    width: 800,
-    height: 599,
-  },
-  {
-    src: 'https://source.unsplash.com/I1ASdgphUH4/800x599',
-    width: 800,
-    height: 599,
+    src: '/img/gallery/2022-06/20220613_202013-srcw.jpg',
+    title: 'PLDI 2022',
+    description:
+      "During PLDI '22. The beach of Catamaran resort hotel.\n2022/06/13. San Diego, California, USA. Taken with Oneplus 8T.\nCopyright © 2022 Ye Shu.",
+    width: 4000,
+    height: 3000,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-06/20220613_202013-1920w.jpg',
+        width: 1920,
+        height: 1440,
+      },
+      {
+        src: '/img/gallery/2022-06/20220613_202013-1440w.jpg',
+        width: 1440,
+        height: 1080,
+      },
+      {
+        src: '/img/gallery/2022-06/20220613_202013-1024w.jpg',
+        width: 1024,
+        height: 768,
+      },
+      {
+        src: '/img/gallery/2022-06/20220613_202013-640w.jpg',
+        width: 640,
+        height: 480,
+      },
+      {
+        src: '/img/gallery/2022-06/20220613_202013-320w.jpg',
+        width: 320,
+        height: 240,
+      },
+    ],
   },
 ];
 
