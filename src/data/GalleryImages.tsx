2973dffde5349d27a3160917ae517af72d28e9ed
Ye Shu <hello@shuye.dev>
2023-01-07 03:28:04 -0500

    gallery: Add photos from 2022/08 and 10

--- src/data/GalleryImages.tsx
+++ src/data/GalleryImages.tsx
@@ -208,6 +208,286 @@ const images: Image[] = [
       },
     ],
   },
+  {
+    src: '/img/gallery/2022-08/20220811_152843-srcw.jpg',
+    title: 'Inside Thompson Memorial Chapel',
+    description:
+      'Inside Thompson Memorial Chapel, where my friend played the pipe organ.\n2022/08/11. Williamstown, MA, USA. Taken with iPhone 13.\nCopyright © 2022 Ye Shu.',
+    width: 4032,
+    height: 3024,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-08/20220811_152843-srcw.jpg',
+        width: 4032,
+        height: 3024,
+      },
+      {
+        src: '/img/gallery/2022-08/20220811_152843-1920w.jpg',
+        width: 1920,
+        height: 1440,
+      },
+      {
+        src: '/img/gallery/2022-08/20220811_152843-1440w.jpg',
+        width: 1440,
+        height: 1080,
+      },
+      {
+        src: '/img/gallery/2022-08/20220811_152843-1024w.jpg',
+        width: 1024,
+        height: 768,
+      },
+      {
+        src: '/img/gallery/2022-08/20220811_152843-640w.jpg',
+        width: 640,
+        height: 480,
+      },
+      {
+        src: '/img/gallery/2022-08/20220811_152843-320w.jpg',
+        width: 320,
+        height: 240,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-08/20220812_050522-srcw.jpg',
+    title: 'Before Sunrise',
+    description:
+      'Observing the Perseids, the town, and sunrise from the top of Stone Hill.\n2022/08/12. Williamstown, MA, USA. Taken with iPhone 13.\nCopyright © 2022 Ye Shu.',
+    width: 4032,
+    height: 3024,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-08/20220812_050522-srcw.jpg',
+        width: 4032,
+        height: 3024,
+      },
+      {
+        src: '/img/gallery/2022-08/20220812_050522-1920w.jpg',
+        width: 1920,
+        height: 1440,
+      },
+      {
+        src: '/img/gallery/2022-08/20220812_050522-1440w.jpg',
+        width: 1440,
+        height: 1080,
+      },
+      {
+        src: '/img/gallery/2022-08/20220812_050522-1024w.jpg',
+        width: 1024,
+        height: 768,
+      },
+      {
+        src: '/img/gallery/2022-08/20220812_050522-640w.jpg',
+        width: 640,
+        height: 480,
+      },
+      {
+        src: '/img/gallery/2022-08/20220812_050522-320w.jpg',
+        width: 320,
+        height: 240,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-08/20220812_052631-srcw.jpg',
+    title: 'Sleeping cows',
+    description:
+      'Cows sleeping on top of stone hill.\n2022/08/12. Williamstown, MA, USA. Taken with iPhone 13.\nCopyright © 2022 Ye Shu.',
+    width: 4032,
+    height: 3024,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-08/20220812_052631-srcw.jpg',
+        width: 4032,
+        height: 3024,
+      },
+      {
+        src: '/img/gallery/2022-08/20220812_052631-1920w.jpg',
+        width: 1920,
+        height: 1440,
+      },
+      {
+        src: '/img/gallery/2022-08/20220812_052631-1440w.jpg',
+        width: 1440,
+        height: 1080,
+      },
+      {
+        src: '/img/gallery/2022-08/20220812_052631-1024w.jpg',
+        width: 1024,
+        height: 768,
+      },
+      {
+        src: '/img/gallery/2022-08/20220812_052631-640w.jpg',
+        width: 640,
+        height: 480,
+      },
+      {
+        src: '/img/gallery/2022-08/20220812_052631-320w.jpg',
+        width: 320,
+        height: 240,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-08/20220813_020353-srcw.jpg',
+    title: 'Perseids 2022',
+    description:
+      'The Perseids 2022. Observe a plane flying over and a small meteor.\n2022/08/12. Williamstown, MA, USA. Taken with Nikon D90 and Nikkor 10.5mm f/2.8.\nCopyright © 2022 Ye Shu.',
+    width: 4288,
+    height: 2848,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-08/20220813_020353-srcw.jpg',
+        width: 4288,
+        height: 2848,
+      },
+      {
+        src: '/img/gallery/2022-08/20220813_020353-1920w.jpg',
+        width: 1920,
+        height: 1275,
+      },
+      {
+        src: '/img/gallery/2022-08/20220813_020353-1440w.jpg',
+        width: 1440,
+        height: 956,
+      },
+      {
+        src: '/img/gallery/2022-08/20220813_020353-1024w.jpg',
+        width: 1024,
+        height: 680,
+      },
+      {
+        src: '/img/gallery/2022-08/20220813_020353-640w.jpg',
+        width: 640,
+        height: 425,
+      },
+      {
+        src: '/img/gallery/2022-08/20220813_020353-320w.jpg',
+        width: 320,
+        height: 212,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-08/20220813_KG7794-srcw.jpg',
+    title: 'Me photographing',
+    description:
+      'Me photographing the Perseids. Taken by Kurt Gu.\n2022/08/13. Williamstown, MA, USA.\nCopyright © 2022 Zhongyi (Kurt) Gu.',
+    width: 3906,
+    height: 4883,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-08/20220813_KG7794-srcw.jpg',
+        width: 3906,
+        height: 4883,
+      },
+      {
+        src: '/img/gallery/2022-08/20220813_KG7794-1920w.jpg',
+        width: 1920,
+        height: 2400,
+      },
+      {
+        src: '/img/gallery/2022-08/20220813_KG7794-1440w.jpg',
+        width: 1440,
+        height: 1800,
+      },
+      {
+        src: '/img/gallery/2022-08/20220813_KG7794-1024w.jpg',
+        width: 1024,
+        height: 1280,
+      },
+      {
+        src: '/img/gallery/2022-08/20220813_KG7794-640w.jpg',
+        width: 640,
+        height: 800,
+      },
+      {
+        src: '/img/gallery/2022-08/20220813_KG7794-320w.jpg',
+        width: 320,
+        height: 400,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-10/20221010_165351-srcw.jpg',
+    title: 'The new Williamstown Bike Trail',
+    description:
+      'The new Williamstown Bike Trail. 2022/10/10. Williamstown, MA, USA. Taken with iPhone 13.\nCopyright © 2022 Ye Shu.',
+    width: 3024,
+    height: 4032,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-10/20221010_165351-srcw.jpg',
+        width: 3024,
+        height: 4032,
+      },
+      {
+        src: '/img/gallery/2022-10/20221010_165351-1920w.jpg',
+        width: 1920,
+        height: 2560,
+      },
+      {
+        src: '/img/gallery/2022-10/20221010_165351-1440w.jpg',
+        width: 1440,
+        height: 1920,
+      },
+      {
+        src: '/img/gallery/2022-10/20221010_165351-1024w.jpg',
+        width: 1024,
+        height: 1365,
+      },
+      {
+        src: '/img/gallery/2022-10/20221010_165351-640w.jpg',
+        width: 640,
+        height: 853,
+      },
+      {
+        src: '/img/gallery/2022-10/20221010_165351-320w.jpg',
+        width: 320,
+        height: 426,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-10/20221014_165942-srcw.jpg',
+    title: 'Sunset on Mount Greylock. Mountain Day 2022.',
+    description:
+      'Sunset on Mount Greylock. Mountain Day 2022. 2022/10/14. Williamstown, MA, USA. Taken with iPhone 13.\nCopyright © 2022 Ye Shu.',
+    width: 3024,
+    height: 4032,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-10/20221014_165942-srcw.jpg',
+        width: 3024,
+        height: 4032,
+      },
+      {
+        src: '/img/gallery/2022-10/20221014_165942-1920w.jpg',
+        width: 1920,
+        height: 2560,
+      },
+      {
+        src: '/img/gallery/2022-10/20221014_165942-1440w.jpg',
+        width: 1440,
+        height: 1920,
+      },
+      {
+        src: '/img/gallery/2022-10/20221014_165942-1024w.jpg',
+        width: 1024,
+        height: 1365,
+      },
+      {
+        src: '/img/gallery/2022-10/20221014_165942-640w.jpg',
+        width: 640,
+        height: 853,
+      },
+      {
+        src: '/img/gallery/2022-10/20221014_165942-320w.jpg',
+        width: 320,
+        height: 426,
+      },
+    ],
+  },
 ];
 
 export default images;
