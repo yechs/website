bf1b0fdd25ac7868ed4fde7b9fc3c6bcb1cdeb3a
Ye Shu <hello@shuye.dev>
2023-01-11 14:32:51 -0500

    gallery: Add photos from 2022/11

--- src/data/GalleryImages.tsx
+++ src/data/GalleryImages.tsx
@@ -488,6 +488,526 @@ const images: Image[] = [
       },
     ],
   },
+  {
+    src: '/img/gallery/2022-11/20221108_015920-srcw.jpg',
+    title: 'Deer on Stetson Court',
+    description:
+      'Deer on Stetson Court. 2022/11/08. Williamstown, MA, USA. Taken with iPhone 13.\nCopyright © 2022 Ye Shu.',
+    width: 4032,
+    height: 3024,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-11/20221108_015920-srcw.jpg',
+        width: 4032,
+        height: 3024,
+      },
+      {
+        src: '/img/gallery/2022-11/20221108_015920-1920w.jpg',
+        width: 1920,
+        height: 1440,
+      },
+      {
+        src: '/img/gallery/2022-11/20221108_015920-1440w.jpg',
+        width: 1440,
+        height: 1080,
+      },
+      {
+        src: '/img/gallery/2022-11/20221108_015920-1024w.jpg',
+        width: 1024,
+        height: 768,
+      },
+      {
+        src: '/img/gallery/2022-11/20221108_015920-640w.jpg',
+        width: 640,
+        height: 480,
+      },
+      {
+        src: '/img/gallery/2022-11/20221108_015920-320w.jpg',
+        width: 320,
+        height: 240,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-11/20221112_145053-srcw.jpg',
+    title: 'Homecoming Day 2022',
+    description:
+      'After the match that we beat Amherst by 20-10. Homecoming Day 2022. Williamstown, MA, USA. Taken with iPhone 13. \nCopyright © 2022 Ye Shu.',
+    width: 4032,
+    height: 3024,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-11/20221112_145053-srcw.jpg',
+        width: 4032,
+        height: 3024,
+      },
+      {
+        src: '/img/gallery/2022-11/20221112_145053-1920w.jpg',
+        width: 1920,
+        height: 1440,
+      },
+      {
+        src: '/img/gallery/2022-11/20221112_145053-1440w.jpg',
+        width: 1440,
+        height: 1080,
+      },
+      {
+        src: '/img/gallery/2022-11/20221112_145053-1024w.jpg',
+        width: 1024,
+        height: 768,
+      },
+      {
+        src: '/img/gallery/2022-11/20221112_145053-640w.jpg',
+        width: 640,
+        height: 480,
+      },
+      {
+        src: '/img/gallery/2022-11/20221112_145053-320w.jpg',
+        width: 320,
+        height: 240,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-11/20221125_073752-srcw.jpg',
+    title: 'Trail Ridge Road in Winter',
+    description:
+      'The closed part of Trail Ridge Road. 2022/11/25. Rocky Mountain National Park, CO, USA. Taken with iPhone 13.\nCopyright © 2022 Ye Shu.',
+    width: 4032,
+    height: 3024,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-11/20221125_073752-srcw.jpg',
+        width: 4032,
+        height: 3024,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_073752-1920w.jpg',
+        width: 1920,
+        height: 1440,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_073752-1440w.jpg',
+        width: 1440,
+        height: 1080,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_073752-1024w.jpg',
+        width: 1024,
+        height: 768,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_073752-640w.jpg',
+        width: 640,
+        height: 480,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_073752-320w.jpg',
+        width: 320,
+        height: 240,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-11/20221125_162828-srcw.jpg',
+    title: 'Sunset in Rocky Mountains',
+    description:
+      'Evening Mountains. 2022/11/25. Sheep Lakes, Rocky Mountain National Park, CO, USA. Taken with iPhone 13 over Nikon Action EX 10x50.\nCopyright © 2022 Ye Shu.',
+    width: 3024,
+    height: 4032,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-11/20221125_162828-srcw.jpg',
+        width: 3024,
+        height: 4032,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_162828-1920w.jpg',
+        width: 1920,
+        height: 2560,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_162828-1440w.jpg',
+        width: 1440,
+        height: 1920,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_162828-1024w.jpg',
+        width: 1024,
+        height: 1365,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_162828-640w.jpg',
+        width: 640,
+        height: 853,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_162828-320w.jpg',
+        width: 320,
+        height: 426,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-11/20221125_163048-srcw.jpg',
+    title: 'Several Mule Deer Bucks',
+    description:
+      'Several Mule Deer (?) Bucks. 2022/11/25. Sheep Lakes, Rocky Mountain National Park, CO, USA. Taken with iPhone 13 over Nikon Action EX 10x50.\nCopyright © 2022 Ye Shu.',
+    width: 3024,
+    height: 4032,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-11/20221125_163048-srcw.jpg',
+        width: 3024,
+        height: 4032,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_163048-1920w.jpg',
+        width: 1920,
+        height: 2560,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_163048-1440w.jpg',
+        width: 1440,
+        height: 1920,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_163048-1024w.jpg',
+        width: 1024,
+        height: 1365,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_163048-640w.jpg',
+        width: 640,
+        height: 853,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_163048-320w.jpg',
+        width: 320,
+        height: 426,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-11/20221125_170656-srcw.jpg',
+    title: 'Mule Deer Doe.',
+    description:
+      'A Mule Deer (?) Doe on roadside. 2022/11/25. US-36 (near Bear Lake Rd), Rocky Mountain National Park, CO, USA. Taken with iPhone 13 over Nikon Action EX 10x50.\nCopyright © 2022 Ye Shu.',
+    width: 3024,
+    height: 4032,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-11/20221125_170656-srcw.jpg',
+        width: 3024,
+        height: 4032,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_170656-1920w.jpg',
+        width: 1920,
+        height: 2560,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_170656-1440w.jpg',
+        width: 1440,
+        height: 1920,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_170656-1024w.jpg',
+        width: 1024,
+        height: 1365,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_170656-640w.jpg',
+        width: 640,
+        height: 853,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_170656-320w.jpg',
+        width: 320,
+        height: 426,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-11/20221125_234550-srcw.jpg',
+    title: 'Night sky in Rocky Mountains',
+    description:
+      'Night Sky and Milky Way. 2022/11/25. Beaver Meadows Entrance, Rocky Mountain National Park, CO, USA. Taken with iPhone 13. 26mm f1.6 ISO5000 3.0s.\nCopyright © 2022 Ye Shu.',
+    width: 4032,
+    height: 3024,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-11/20221125_234550-srcw.jpg',
+        width: 4032,
+        height: 3024,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_234550-1920w.jpg',
+        width: 1920,
+        height: 1440,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_234550-1440w.jpg',
+        width: 1440,
+        height: 1080,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_234550-1024w.jpg',
+        width: 1024,
+        height: 768,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_234550-640w.jpg',
+        width: 640,
+        height: 480,
+      },
+      {
+        src: '/img/gallery/2022-11/20221125_234550-320w.jpg',
+        width: 320,
+        height: 240,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-11/20221126_001121-srcw.jpg',
+    title: 'Night sky in Rocky Mountains x2',
+    description:
+      'Night Sky and Orion. 2022/11/26. Beaver Meadows Entrance, Rocky Mountain National Park, CO, USA. Taken with iPhone 13. 26mm f1.6 ISO10000 1.0s.\nCopyright © 2022 Ye Shu.',
+    width: 3024,
+    height: 4032,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-11/20221126_001121-srcw.jpg',
+        width: 3024,
+        height: 4032,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_001121-1920w.jpg',
+        width: 1920,
+        height: 2560,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_001121-1440w.jpg',
+        width: 1440,
+        height: 1920,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_001121-1024w.jpg',
+        width: 1024,
+        height: 1365,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_001121-640w.jpg',
+        width: 640,
+        height: 853,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_001121-320w.jpg',
+        width: 320,
+        height: 426,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-11/20221126_003154-srcw.jpg',
+    title: 'Night sky in Rocky Mountains x3',
+    description:
+      'Night Sky. 2022/11/26. Beaver Meadows Entrance, Rocky Mountain National Park, CO, USA. Taken with Pixel 6. 6.81mm f1.85 ISO3272 16.0s.\nCopyright © 2022 Alex Meng.',
+    width: 4080,
+    height: 3072,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-11/20221126_003154-srcw.jpg',
+        width: 4080,
+        height: 3072,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_003154-1920w.jpg',
+        width: 1920,
+        height: 1445,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_003154-1440w.jpg',
+        width: 1440,
+        height: 1084,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_003154-1024w.jpg',
+        width: 1024,
+        height: 771,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_003154-640w.jpg',
+        width: 640,
+        height: 481,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_003154-320w.jpg',
+        width: 320,
+        height: 240,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-11/20221126_112759-srcw.jpg',
+    title: 'Frozen Lake and Powerplant',
+    description:
+      'Frozen lake and Marys Lake Powerplant. 2022/11/26. Marys Lake, Estes Park, CO, USA. Taken with iPhone 13.\nCopyright © 2022 Ye Shu.',
+    width: 4032,
+    height: 3024,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-11/20221126_112759-srcw.jpg',
+        width: 4032,
+        height: 3024,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_112759-1920w.jpg',
+        width: 1920,
+        height: 1440,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_112759-1440w.jpg',
+        width: 1440,
+        height: 1080,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_112759-1024w.jpg',
+        width: 1024,
+        height: 768,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_112759-640w.jpg',
+        width: 640,
+        height: 480,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_112759-320w.jpg',
+        width: 320,
+        height: 240,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-11/20221126_150343-srcw.jpg',
+    title: 'Bison in RMA Wildlife Refuge',
+    description:
+      'Bison in Rocky Mountain Arsenal National Wildlife Refuge. 2022/11/26. Rocky Mountain Arsenal National Wildlife Refuge, Colorado, CO, USA. Taken with iPhone 13 over Nikon Action EX 10x50.\nCopyright © 2022 Ye Shu.',
+    width: 3024,
+    height: 4032,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-11/20221126_150343-srcw.jpg',
+        width: 3024,
+        height: 4032,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_150343-1920w.jpg',
+        width: 1920,
+        height: 2560,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_150343-1440w.jpg',
+        width: 1440,
+        height: 1920,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_150343-1024w.jpg',
+        width: 1024,
+        height: 1365,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_150343-640w.jpg',
+        width: 640,
+        height: 853,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_150343-320w.jpg',
+        width: 320,
+        height: 426,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-11/20221126_152251-srcw.jpg',
+    title: 'Nature, Culture.',
+    description:
+      'Bisons in front of the city of Colorado. 2022/11/26. Rocky Mountain Arsenal National Wildlife Refuge, Colorado, CO, USA. Taken with iPhone 13 over Nikon Action EX 10x50.\nCopyright © 2022 Ye Shu.',
+    width: 3024,
+    height: 4032,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-11/20221126_152251-srcw.jpg',
+        width: 3024,
+        height: 4032,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_152251-1920w.jpg',
+        width: 1920,
+        height: 2560,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_152251-1440w.jpg',
+        width: 1440,
+        height: 1920,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_152251-1024w.jpg',
+        width: 1024,
+        height: 1365,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_152251-640w.jpg',
+        width: 640,
+        height: 853,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_152251-320w.jpg',
+        width: 320,
+        height: 426,
+      },
+    ],
+  },
+  {
+    src: '/img/gallery/2022-11/20221126_152626-srcw.jpg',
+    title: 'Just a Bunker',
+    description:
+      'A former US Army bunker with broken windows. 2022/11/26. Tiny Bunker, Rocky Mountain Arsenal National Wildlife Refuge, Colorado, CO, USA. Taken with iPhone 13.\nCopyright © 2022 Ye Shu.',
+    width: 4032,
+    height: 3024,
+    srcSet: [
+      {
+        src: '/img/gallery/2022-11/20221126_152626-srcw.jpg',
+        width: 4032,
+        height: 3024,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_152626-1920w.jpg',
+        width: 1920,
+        height: 1440,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_152626-1440w.jpg',
+        width: 1440,
+        height: 1080,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_152626-1024w.jpg',
+        width: 1024,
+        height: 768,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_152626-640w.jpg',
+        width: 640,
+        height: 480,
+      },
+      {
+        src: '/img/gallery/2022-11/20221126_152626-320w.jpg',
+        width: 320,
+        height: 240,
+      },
+    ],
+  },
 ];
 
 export default images;
